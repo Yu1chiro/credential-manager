5ba80c8882e233b8473e8ffb38c08853f59c862d
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-02-28 03:26:44 +0800

    update guide

--- public/service/intro.js
+++ public/service/intro.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const steps = [
+        { element: null, intro: 'Selamat datang di Credential Manager, Yuk pelajari fitur"nya!' },
         { element: '#readme', intro: 'Memuat informasi terkait credential manager, silakan baca dokumentasi berikut.' },
         { element: '#important', intro: 'Masukkan informasi credential akun Anda seperti Gmail, sosial media, email pemulihan, dan email sekolah.' },
         { element: '#output', intro: 'Tambahkan gambar untuk mempercantik tampilan barcode Anda!' },
